206ebefd33c7a3172805d5ddb9c9a97a2bd69724
David M <david.mitre99@gmail.com>
2024-01-07 22:45:23 +0200

    Added initial task scheduler

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ async function connectToDb() {
   try {
     await client.connect();
     db_users = client.db('silk-scraper-users');
+    db_tasks = client.db('silk-scraper-tasks');
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Could not connect to DB:', error);
@@ -63,6 +64,26 @@ app.get('/api/getUserByEmail', async (req, res) => {
         console.error(error);
         res.status(500).send('Internal Server Error');
     }
+});
+
+app.post('/api/scheduleTask', async (req, res) => {
+  const {userEmail, taskData} = req.body;
+
+  try {
+    const tasksCollection = db_tasks.collection('tasks');
+    const scheduleTime = new Date(taskData.datetime).toISOString();
+
+    const usersCollection = db_users.collection('users');
+    const user = await usersCollection.findOne({ email: userEmail });
+
+    const newTask = {uid: user._id , scheduleTime, taskDetails: taskData, status: "scheduled"};
+    await tasksCollection.insertOne(newTask);
+
+    res.status(200).send({ message: "Task scheduled successfully" });
+  } catch (error) {
+    console.error("Error scheduling task:", error);
+    res.status(500).send({ message: "Failed to schedule task" });
+  }
 });
 
 app.listen(port, () => {
